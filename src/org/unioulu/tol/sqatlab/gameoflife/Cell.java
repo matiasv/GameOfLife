769fc5779c64afe4912aacb0ffcb92fe92c55db1
somename <someemail>
2015-10-08 12:28:10 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Cell.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Cell.java
@@ -11,8 +11,17 @@ public class Cell {
 		return state;
 	}
 	
-	public void update(int liveNeighours) {
+	public void update(int liveNeigbhours) {
+		if(this.getState() == "Alive") {
+			updateLiveCell(liveNeigbhours);
+		}
 		if(liveNeighours == 0) {
+			this.state = "Dead";
+		}
+	}
+	
+	private void updateLiveCell(int liveNeighbours) {
+		if(liveNeighbours < 2) {
 			this.state = "Dead";
 		}
 	}
